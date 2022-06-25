81bb8f1feaa723f627c150089ade5af8e5473870
novaardiansyah <novaardiansyah78@gmail.com>
2022-06-25 23:23:33 +0700

    Studying Variables with string data type

--- main.go
+++ main.go
@@ -4,4 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	var fullname string = "Nova Ardiansyah"
+	fmt.Println(fullname)
+
+	fullname = "First Variable"
+	fmt.Println(fullname)
+
+	company := "Nova INC"
+	fmt.Println(company)
 }
