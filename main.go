a19abf743ce6877f5563b7184448d6b975259290
novaardiansyah <novaardiansyah78@gmail.com>
2022-06-26 08:49:02 +0700

    Integer and Float

--- main.go
+++ main.go
@@ -13,4 +13,22 @@ func main() {
 
 	company := "Nova INC"
 	fmt.Println(company)
+
+	// integer
+	var ageOne int = 21
+	var ageTwo = 22
+	ageThree := 23
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	// bit and memory
+	var numOne int8 = 127
+	var numTwo int8 = -128
+	fmt.Println(numOne, numTwo)
+
+	var numTrie uint8 = 255
+	fmt.Println(numTrie)
+
+	// float
+	var numFloat float32 = 3.14
+	fmt.Println(numFloat)
 }
