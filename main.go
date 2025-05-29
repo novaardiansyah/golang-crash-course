39f6c233a097a78e8196f225ede4e07ad716e6ec
novaardiansyah <novaardiansyah78@gmail.com>
2025-05-29 17:49:36 +0700

    If and else conditions

--- main.go
+++ main.go
@@ -2,26 +2,14 @@ package main
 
 import (
 	"fmt"
-	"golang-crash-course/calc"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
-	sentence := testString()
-	fmt.Println(sentence)
-
-	result := calc.Add(3, 4)
-	fmt.Printf("The result of adding 3 and 4 is: %d\n", result)
-
-	multiply := calc.Multiply(3, 4)
-	fmt.Printf("The result of multiplying 3 and 4 is: %d\n", multiply)
-
-	var color string = "blue"
-	fmt.Printf("The color is: %s\n", color)
-
 	age := 30
-	fmt.Printf("The age is: %d\n", age)
 
-	score := 95.5
-	fmt.Printf("The score is: %.1f\n", score)
+	if age < 18 {
+		fmt.Println("You are a minor.")
+	} else {
+		fmt.Println("You are an adult.")
+	}
 }
