5fffe173d53b89a60304451548f612846aba3750
novaardiansyah <novaardiansyah78@gmail.com>
2025-05-29 17:48:49 +0700

    Variables, Data Types, and Default Values

--- main.go
+++ main.go
@@ -15,4 +15,13 @@ func main() {
 
 	multiply := calc.Multiply(3, 4)
 	fmt.Printf("The result of multiplying 3 and 4 is: %d\n", multiply)
+
+	var color string = "blue"
+	fmt.Printf("The color is: %s\n", color)
+
+	age := 30
+	fmt.Printf("The age is: %d\n", age)
+
+	score := 95.5
+	fmt.Printf("The score is: %.1f\n", score)
 }
