a5b1d1cb29fd2c122093b31961c4acafdfafd62a
novaardiansyah <novaardiansyah78@gmail.com>
2025-05-29 17:26:54 +0700

    Advanced Package

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"golang-crash-course/calc"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
 	sentence := testString()
 	fmt.Println(sentence)
+
+	result := calc.Add(3, 4)
+	fmt.Printf("The result of adding 3 and 4 is: %d\n", result)
 }
