ef4838acb67b016409e0340a727fd2dbee4d932f
novaardiansyah <novaardiansyah78@gmail.com>
2025-05-29 17:53:56 +0700

    For loop and range

--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import (
 )
 
 func main() {
-	color := "red"
-	
-	switch {
-		case color == "red":
-			fmt.Println("The color is red.")
-		default:
-			fmt.Println("The color is not red.")
+	// ! Simple for loop
+	for i := 0; i < 5; i++ {
+		fmt.Println("Iteration:", i)
+	}
+
+	// ! Using a range loop
+	for i := range 5 {
+		fmt.Println("Range iteration:", i)
 	}
 }
